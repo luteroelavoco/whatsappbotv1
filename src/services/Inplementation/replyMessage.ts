3a4f91ad741350fd0ab92d2752fd009c18e34a56
lutero.elavoco <lutero.elavoco@mutual.club>
2022-04-08 02:09:50 -0300

    feat: add new version

--- src/services/Inplementation/replyMessage.ts
+++ src/services/Inplementation/replyMessage.ts
@@ -1,15 +1,15 @@
-import { IReplyMessage } from "../IReplyMessage";
-import axios from "axios";
-import { getAnswer } from "./naturalLanguage";
+import axios from 'axios';
 import {
   handleNewsPapper,
   isNewsAvalaible,
   isNewsMessage,
-} from "@utils/message";
-import { navigatorsType } from "@mytypes/navigators";
+} from '@utils/message';
+import { navigatorsType } from '@mytypes/navigators';
+import { getAnswer } from './naturalLanguage';
+import { IReplyMessage } from '../IReplyMessage';
 
-const YoutubeLink = "https://www.youtube.com/channel/UCnRkmcr5V0Ye5kh1hE5yL2Q";
-const InstagramLink = "https://www.instagram.com/luteroelavoco1"
+const YoutubeLink = 'https://www.youtube.com/channel/UCnRkmcr5V0Ye5kh1hE5yL2Q';
+const InstagramLink = 'https://www.instagram.com/luteroelavoco1';
 
 const instance = axios.create({
   baseURL: 'https://newswebscrapingapi.vercel.app/api/news/',
@@ -17,12 +17,7 @@ const instance = axios.create({
 });
 
 export async function onMessageAnswer(question) {
-  return await getAnswer(question);
-}
-
-async function sendNewsMessage(client, message) {
-  client.sendText(message.from, "Aguarde... Estou buscando noticias para você");
-  getNewsAndAnswer(client, message, handleNewsPapper(message.body));
+  getAnswer(question);
 }
 
 async function getNewsAndAnswer(client, message, newspapper: string) {
@@ -32,8 +27,8 @@ async function getNewsAndAnswer(client, message, newspapper: string) {
     .then(async (response) => {
       const { data } = response;
 
-      for (var i = 0; i < data.linkList.length; i++) {
-        let item = data.linkList[i];
+      for (let i = 0; i < data.linkList.length; i++) {
+        const item = data.linkList[i];
         client.sendLinkPreview(message.from, item.href, data.source);
       }
     })
@@ -41,9 +36,13 @@ async function getNewsAndAnswer(client, message, newspapper: string) {
       console.log(ex);
       client.sendText(
         message.from,
-        "Desculpa ! Não consegui buscar notícias. Você pode tentar novamente ?"
+        'Desculpa ! Não consegui buscar notícias. Você pode tentar novamente ?',
       );
     });
+}
+async function sendNewsMessage(client, message) {
+  client.sendText(message.from, 'Aguarde... Estou buscando noticias para você');
+  getNewsAndAnswer(client, message, handleNewsPapper(message.body));
 }
 
 export class ReplyMessage implements IReplyMessage {
@@ -55,63 +54,60 @@ export class ReplyMessage implements IReplyMessage {
       else {
         client.sendText(
           message.from,
-          "Esta fonte de notícia estará disponivel apenas em 8 de Abril de 2022. Acompanha meu canal do youtube e meu instagram para mais informações"
+          'Esta fonte de notícia estará disponivel apenas em 8 de Abril de 2022. Acompanha meu canal do youtube e meu instagram para mais informações',
         );
-         client.sendLinkPreview(
+        client.sendLinkPreview(
           message.from,
           InstagramLink,
-          "Meu Instagram oficial"
+          'Meu Instagram oficial',
         );
-         client.sendLinkPreview(
+        client.sendLinkPreview(
           message.from,
           YoutubeLink,
-          "Meu canal do youtube"
+          'Meu canal do youtube',
         );
-       
       }
     } else {
       const messageToAnswer = await onMessageAnswer(message.body);
       const list = [
         {
-          title: "Fontes disponiveis",
+          title: 'Fontes disponiveis',
           rows: [
             {
-              title: "BBB",
-              description: " ",
+              title: 'BBB',
+              description: ' ',
             },
             {
-              title: "Folha de São Paulo",
-              description: " ",
+              title: 'Folha de São Paulo',
+              description: ' ',
             },
             {
-              title: "O Povo",
-              description: " ",
+              title: 'O Povo',
+              description: ' ',
             },
             {
-              title: "O Globo",
-              description: " ",
+              title: 'O Globo',
+              description: ' ',
             },
-            // {
-            //   title: "CNN",
-            //   description: " ",
-            // },
-            // {
-            //   title: "Jornal de Angola",
-            //   description: " ",
-            // },
+            {
+              title: 'CNN',
+              description: ' ',
+            },
+            {
+              title: 'Jornal de Angola',
+              description: ' ',
+            },
           ],
         },
       ];
-      client
-        .sendListMenu(
-          message.from,
-          "WhatsappNews",
-          "#NoFakeNews",
-          messageToAnswer,
-          "Escolher fonte",
-          list
-        )
-       
+      client.sendListMenu(
+        message.from,
+        'WhatsappNews',
+        '#NoFakeNews',
+        messageToAnswer,
+        'Escolher fonte',
+        list,
+      );
     }
   }
 }
--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,20 +1,17 @@
 export function isNewsMessage(message: string) {
-  let search = '' + message.toLocaleLowerCase();
-  return search.includes('folha') || search.includes('povo') || search.includes('globo') || search.includes('bbb');
+  const search = `${message.toLocaleLowerCase()}`;
+  return search.includes('folha') || search.includes('povo') || search.includes('globo') || search.includes('bbb') || search.includes('angola') || search.includes('cnn');
 }
 
 export function isNewsAvalaible(message: string) {
-  let search = '' + message.toLocaleLowerCase();
+  const search = `${search.includes('folha')}` || search.includes('povo') || search.includes('globo') || search.includes('bbb');
   return search.includes('bbb');
 }
 
 export function handleNewsPapper(message: string) {
   message = message.toLowerCase();
-  if(message.includes('povo'))
-    return "povo";
-  if(message.includes('globo'))
-    return "globo";
-  if(message.includes('bbb'))
-    return "bbb";
-  return "folha";
+  if (message.includes('povo')) { return 'povo'; }
+  if (message.includes('globo')) { return 'globo'; }
+  if (message.includes('bbb')) { return 'bbb'; }
+  return 'folha';
 }
